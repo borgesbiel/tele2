a312055af7fc1eec065073b92762fb30fd987893
Gabriel <gabriel.bidula@gmail.com>
2017-11-11 21:16:48 +0100

    add bunny connector class

---
+++ lib/bunny_connector.rb
@@ -0,0 +1,10 @@
+class BunnyConnector
+  class << self
+    def new
+      conn = Bunny.new Configuration.rabbitmq_url
+      conn.start
+    rescue Exception => e
+      puts "Exception: #{e.message}"
+    end
+  end
+end
