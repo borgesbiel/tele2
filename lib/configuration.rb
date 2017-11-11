1ed3dc8d784b9208279cde33da214d71830c0ec1
Gabriel <gabriel.bidula@gmail.com>
2017-11-11 20:20:40 +0100

    refactor to class

--- lib/configuration.rb
+++ lib/configuration.rb
@@ -1,9 +1,11 @@
 class Configuration
-  def self.rabbitmq_url
-    default_url
-  end
+  class << self
+    def rabbitmq_url
+      default_url
+    end
 
-  def self.default_url
-    "amqp://guest:guest@localhost:5672"
+    def default_url
+      "amqp://guest:guest@localhost:5672"
+    end
   end
 end
--- lib/keyboard_client.rb
+++ lib/keyboard_client.rb
@@ -3,26 +3,30 @@ require 'rubygems'
 require 'eventmachine'
 require_relative 'configuration'
 
-module MyKeyboardHandler
-  include EM::Protocols::LineText2
-  def receive_line data
-    conn = Bunny.new Configuration.rabbitmq_url, automatically_recover: false
-    conn.start
+class KeyboardClient
+  module MyKeyboardHandler
+    include EM::Protocols::LineText2
 
-    ch   = conn.create_channel
-    q    = ch.queue 'gateway_queue'
+    def receive_line data
+      conn = Bunny.new Configuration.rabbitmq_url
+      conn.start
 
-    ch.default_exchange.publish data, routing_key: q.name, persistent: true
-    puts " [x] Keyboard Sent '#{data}'"
+      ch = conn.create_channel
+      q  = ch.queue 'gateway_queue'
 
-    conn.close
+      ch.default_exchange.publish data, routing_key: q.name, persistent: true
+      puts " [x] Keyboard Sent '#{data}'"
+
+      conn.close
+    end
   end
-end
 
-EM.run do
-  puts ' [*] Keyboard client ------'
-  puts ' [*] Type text and press Enter to send a message'
-  puts ' [*] To exit press CTRL+C'
-  puts
-  EM.open_keyboard MyKeyboardHandler
+  EM.run do
+    puts ' [*] Keyboard client ------'
+    puts ' [*] Type text and press Enter to send a message'
+    puts ' [*] To exit press CTRL+C'
+    puts
+
+    EM.open_keyboard MyKeyboardHandler
+  end
 end
