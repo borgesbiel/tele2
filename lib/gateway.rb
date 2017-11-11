c5bac7a8c95957aa24b7015c498921a16bbf15e5
Gabriel <gabriel.bidula@gmail.com>
2017-11-11 21:29:59 +0100

    wip

--- lib/gateway.rb
+++ lib/gateway.rb
@@ -1,40 +1,33 @@
 require 'bunny'
 require_relative 'configuration'
+require_relative 'bunny_connector'
 
-class Gateway
-  conn = Bunny.new
-  conn.start
+def send_message data
+  conn = BunnyConnector.new
+  ch   = conn.create_channel
 
-  ch = conn.create_channel
-  q = ch.queue 'gateway_queue'
+  ex   = ch.fanout 'gateway_exchange'
+  ex.publish data
 
-  begin
-    puts ' [*] Gateway Intermediate service ------'
-    puts ' [*] Waiting for messages...'
-    puts ' [*] To exit press CTRL+C'
+  puts " [x] Mediated:  #{data}"
+  conn.close
+end
 
-    q.subscribe block: true do |_delivery_info, _properties, body|
-      send_message "Mediated: #{body}"
-    end
-  rescue Interrupt => _
-    conn.close
+conn = BunnyConnector.new
+ch   = conn.create_channel
+q    = ch.queue 'gateway_queue'
 
-    exit(0)
+begin
+  puts ' [*] Gateway service ------'
+  puts ' [*] Waiting for messages...'
+  puts ' [*] To exit press CTRL+C'
+  puts
+
+  q.subscribe block: true do |_delivery_info, _properties, body|
+    send_message "#{body}"
   end
+rescue Interrupt => _
+  conn.close
 
-  private
-
-  def send_message data
-    conn = Bunny.new
-    conn.start
-
-    ch = conn.create_channel
-    ex = ch.fanout 'gateway_exchange'
-
-    ex.publish data
-
-    puts " [x] Mediating '#{data}'"
-
-    conn.close
-  end
+  exit(0)
 end
--- lib/keyboard_client.rb
+++ lib/keyboard_client.rb
@@ -2,27 +2,26 @@ require 'bunny'
 require 'rubygems'
 require 'eventmachine'
 require_relative 'configuration'
+require_relative 'bunny_connector'
 
 class KeyboardClient
   module MyKeyboardHandler
     include EM::Protocols::LineText2
 
     def receive_line data
-      conn = Bunny.new Configuration.rabbitmq_url
-      conn.start
-
-      ch = conn.create_channel
-      q  = ch.queue 'gateway_queue'
+      conn = BunnyConnector.new
+      ch   = conn.create_channel
+      q    = ch.queue 'gateway_queue'
 
       ch.default_exchange.publish data, routing_key: q.name, persistent: true
-      puts " [x] Keyboard Sent '#{data}'"
+      puts " [x] Keyboard Sent: #{data}"
 
       conn.close
     end
   end
 
   EM.run do
-    puts ' [*] Keyboard client ------'
+    puts ' [*] Keyboard client service ------'
     puts ' [*] Type text and press Enter to send a message'
     puts ' [*] To exit press CTRL+C'
     puts
